5b49ee57590908a53d99e93e46eb2824f804ad84
Isac <isac08270@gmail.com>
2023-05-02 15:12:18 +1000

    deleted unused import

--- Hitomi-Scroll-Viewer/MainWindow.xaml.cs
+++ Hitomi-Scroll-Viewer/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using System;
-using System.Threading.Tasks;
 
 namespace Hitomi_Scroll_Viewer {
     public sealed partial class MainWindow : Window {
