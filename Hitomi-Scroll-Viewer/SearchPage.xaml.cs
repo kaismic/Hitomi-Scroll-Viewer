ee94c95f0f96621f4c4f3f6d0b852aa5ddee198a
Isac <isac08270@gmail.com>
2023-07-04 18:26:36 +1000

    removed unnecessary directives

--- Hitomi-Scroll-Viewer/SearchPage.xaml.cs
+++ Hitomi-Scroll-Viewer/SearchPage.xaml.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using Windows.ApplicationModel.DataTransfer;
