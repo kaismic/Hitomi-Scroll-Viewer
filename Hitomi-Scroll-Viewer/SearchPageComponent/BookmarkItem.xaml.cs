73c9bf10d0fe1668a43058c90530dbe0088cc117
Isac <isac08270@gmail.com>
2023-07-05 02:22:44 +1000

    BookmarkItem now displays artists name

--- Hitomi-Scroll-Viewer/SearchPageComponent/BookmarkItem.xaml.cs
+++ Hitomi-Scroll-Viewer/SearchPageComponent/BookmarkItem.xaml.cs
@@ -1,35 +1,44 @@
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 using System.IO;
+using Windows.UI.Text;
 using static Hitomi_Scroll_Viewer.MainWindow;
 using static Hitomi_Scroll_Viewer.SearchPage;
 
 namespace Hitomi_Scroll_Viewer.SearchPageComponent {
     public sealed partial class BookmarkItem : StackPanel {
-        public readonly Gallery bmGallery;
+        public readonly Gallery gallery;
         private readonly Image[] _images;
         private readonly HyperlinkButton _hb;
         private readonly bool _imageNotFound = false;
         public BookmarkItem(Gallery newGallery, SearchPage sp) {
             InitializeComponent();
 
-            bmGallery = newGallery;
+            gallery = newGallery;
 
             Orientation = Orientation.Vertical;
             BorderBrush = new SolidColorBrush(Colors.Black);
             BorderThickness = new Thickness(1);
 
-            _hb = new() {
-                Content = new TextBlock() {
-                    Text = bmGallery.title + Environment.NewLine + bmGallery.id,
-                    TextWrapping = TextWrapping.WrapWholeWords,
-                    FontSize = 24,
-                },
+            TextBlock hbText = new() {
+                TextWrapping = TextWrapping.WrapWholeWords,
+                FontSize = 24,
             };
+            string artists = gallery.GetArtists();
+            if (artists == null) {
+                hbText.Inlines.Add(new Run() { Text = gallery.title + Environment.NewLine });
+                hbText.Inlines.Add(new Run() { Text = "N/A" + Environment.NewLine, FontStyle = FontStyle.Italic });
+                hbText.Inlines.Add(new Run() { Text = gallery.id });
+            } else {
+                hbText.Text = gallery.title + Environment.NewLine + artists + Environment.NewLine + gallery.id;
+            }
+
+            _hb = new() { Content = hbText };
             _hb.Click += sp.HandleBookmarkClick;
             Children.Add(_hb);
 
@@ -37,7 +46,7 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             Children.Add(imageContainer);
 
             // add thumbnail images
-            if (Directory.Exists(IMAGE_DIR + @"\" + bmGallery.id)) {
+            if (Directory.Exists(IMAGE_DIR + @"\" + gallery.id)) {
                 _images = new Image[THUMBNAIL_IMG_NUM];
                 for (int i = 0; i < THUMBNAIL_IMG_NUM; i++) {
                     imageContainer.ColumnDefinitions.Add(
@@ -45,11 +54,11 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
                             Width = new GridLength(2, GridUnitType.Star)
                         }
                     );
-                    int imgIdx = i * bmGallery.files.Count / THUMBNAIL_IMG_NUM;
+                    int imgIdx = i * gallery.files.Length / THUMBNAIL_IMG_NUM;
                         _images[i] = new() {
-                            Source = new BitmapImage(new(IMAGE_DIR + @"\" + bmGallery.id + @"\" + imgIdx + IMAGE_EXT)),
+                            Source = new BitmapImage(new(IMAGE_DIR + @"\" + gallery.id + @"\" + imgIdx + IMAGE_EXT)),
                             Width = THUMBNAIL_IMG_WIDTH,
-                            Height = THUMBNAIL_IMG_WIDTH * bmGallery.files[i].height / bmGallery.files[i].width,
+                            Height = THUMBNAIL_IMG_WIDTH * gallery.files[i].height / gallery.files[i].width,
                             HorizontalAlignment = HorizontalAlignment.Center
                         };
                     Grid.SetColumn(_images[i], i);
