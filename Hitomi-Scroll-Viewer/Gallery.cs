d2d59a5b80d73bd43e2ae0ffd7d922081c089440
Isac <isac08270@gmail.com>
2023-07-05 02:20:52 +1000

    added GetArtists method

--- Hitomi-Scroll-Viewer/Gallery.cs
+++ Hitomi-Scroll-Viewer/Gallery.cs
@@ -2,41 +2,48 @@
 
 namespace Hitomi_Scroll_Viewer {
     public class Gallery {
-        public string type = "";
-        public string language_localname = "";
-        public string videofilename = "";
-        public string language = "";
-        public string id = "";
-        public List<ImageInfo> files = new();
-        public List<int> related = new();
-        public string title = "";
-        public string japanese_title = "";
-        public List<Dictionary<string, string>> groups = new();
-        public string language_url = "";
-        public List<int> scene_indexes = new();
-        public List<Dictionary<string, string>> artists = new();
-        public List<Dictionary<string, string>> languages = new();
-        public object video = null;
-        public List<Dictionary<string, string>> characters = new();
-        public string date = "";
-        public List<Dictionary<string, string>> parodys = new();
-        public List<Dictionary<string, object>> tags = new();
+        public string type;
+        public string language_localname;
+        public string videofilename;
+        public string language;
+        public string id;
+        public ImageInfo[] files;
+        public int[] related;
+        public string title;
+        public string japanese_title;
+        public Dictionary<string, string>[] groups;
+        public string language_url;
+        public int[] scene_indexes;
+        public Dictionary<string, string>[] artists;
+        public Dictionary<string, string>[] languages;
+        public object video;
+        public Dictionary<string, string>[] characters;
+        public string date;
+        public Dictionary<string, string>[] parodys;
+        public Dictionary<string, object>[] tags;
 
-        public Gallery() {
+        public Gallery() {}
 
+        public string GetArtists() {
+            if (artists == null) {
+                return null;
+            }
+            string result = "";
+            for (int i = 0; i < artists.Length; i++) {
+                result += artists[i]["artist"] + ", ";
+            }
+            return result[..^", ".Length];
         }
     }
 
     public class ImageInfo {
-        public string name = "";
-        public int height = 0;
-        public int width = 0;
-        public int haswebp = 1;
-        public int hasavif = 1;
-        public string hash = "";
+        public string name;
+        public int height;
+        public int width;
+        public int haswebp;
+        public int hasavif;
+        public string hash;
 
-        public ImageInfo() {
-
-        }
+        public ImageInfo() {}
     }
 }
