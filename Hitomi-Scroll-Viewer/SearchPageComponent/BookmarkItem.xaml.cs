49e6221728eedce9bac76945cc6dd805937029c4
Isac Kim <isac08270@gmail.com>
2023-09-09 22:44:32 +1000

    now inherits Grid instead and added more methods

--- Hitomi-Scroll-Viewer/SearchPageComponent/BookmarkItem.xaml.cs
+++ Hitomi-Scroll-Viewer/SearchPageComponent/BookmarkItem.xaml.cs
@@ -5,14 +5,14 @@ using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.IO;
 using Windows.UI.Text;
 using static Hitomi_Scroll_Viewer.MainWindow;
 using static Hitomi_Scroll_Viewer.SearchPage;
 
 namespace Hitomi_Scroll_Viewer.SearchPageComponent {
-    public sealed partial class BookmarkItem : StackPanel {
+    public sealed partial class BookmarkItem : Grid {
         public readonly Gallery gallery;
+        private readonly Grid _imageContainer;
         private readonly Image[] _images;
         private readonly HyperlinkButton _hb;
         public BookmarkItem(Gallery newGallery, SearchPage sp) {
@@ -20,10 +20,19 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
 
             gallery = newGallery;
 
-            Orientation = Orientation.Vertical;
             BorderBrush = new SolidColorBrush(Colors.Black);
             BorderThickness = new Thickness(1);
 
+            ColumnDefinitions.Add(new() { Width = new GridLength(15, GridUnitType.Star) });
+            ColumnDefinitions.Add(new() { Width = new GridLength(1, GridUnitType.Star) });
+
+            Grid container1 = new() {
+                RowDefinitions = { new RowDefinition(), new RowDefinition() }
+            };
+            Children.Add(container1);
+            SetColumn(container1, 0);
+
+            // add clickable hyperlink which loads bookmarked gallery
             TextBlock hbText = new() {
                 TextWrapping = TextWrapping.WrapWholeWords,
                 FontSize = 24,
@@ -38,47 +47,51 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             }
 
             _hb = new() { Content = hbText };
-            _hb.Click += sp.HandleBookmarkClick;
-            Children.Add(_hb);
+            container1.Children.Add(_hb);
+            _hb.Click += HandleBookmarkClick;
+            SetRow(_hb, 0);
 
-            Grid imageContainer = new();
-            Children.Add(imageContainer);
+            _imageContainer = new();
+            container1.Children.Add(_imageContainer);
+            SetRow(_imageContainer, 1);
 
             // add thumbnail images
             _images = new Image[THUMBNAIL_IMG_NUM];
             for (int i = 0; i < THUMBNAIL_IMG_NUM; i++) {
-                imageContainer.ColumnDefinitions.Add(
-                    new ColumnDefinition() {
-                        Width = new GridLength(2, GridUnitType.Star)
-                    }
-                );
-                int imgIdx = i * gallery.files.Length / THUMBNAIL_IMG_NUM;
-                    _images[i] = new() {
-                        Source = new BitmapImage(new(IMAGE_DIR + @"\" + gallery.id + @"\" + imgIdx + IMAGE_EXT)),
-                        Width = THUMBNAIL_IMG_WIDTH,
-                        Height = THUMBNAIL_IMG_WIDTH * gallery.files[i].height / gallery.files[i].width,
-                        HorizontalAlignment = HorizontalAlignment.Center
-                    };
-                Grid.SetColumn(_images[i], i);
-                imageContainer.Children.Add(_images[i]);
+                _imageContainer.ColumnDefinitions.Add(new());
             }
+            LoadImages();
 
-            // add another ColumnDefinition for remove button
-            imageContainer.ColumnDefinitions.Add(
-                new ColumnDefinition() {
-                    Width = new GridLength(1, GridUnitType.Star)
-                }
-            ); Button removeBtn = new() {
+            // add remove button
+            Button removeBtn = new() {
                 Content = new TextBlock() {
                     Text = "Remove",
                     TextWrapping = TextWrapping.WrapWholeWords,
                 },
                 FontSize = 18,
-                HorizontalAlignment = HorizontalAlignment.Center
             };
-            Grid.SetColumn(removeBtn, THUMBNAIL_IMG_NUM);
+            Children.Add(removeBtn);
             removeBtn.Click += sp.RemoveBookmark;
-            imageContainer.Children.Add(removeBtn);
+            SetColumn(removeBtn, 1);
+        }
+
+        private void HandleBookmarkClick(object _0, RoutedEventArgs _1) {
+            LoadBookmark(gallery);
+        }
+
+        public void LoadImages() {
+            _imageContainer.Children.Clear();
+            for (int i = 0; i < THUMBNAIL_IMG_NUM; i++) {
+                int imgIdx = i * gallery.files.Length / THUMBNAIL_IMG_NUM;
+                _images[i] = new() {
+                    Source = new BitmapImage(new(IMAGE_DIR + DIR_SEP + gallery.id + DIR_SEP + imgIdx + IMAGE_EXT)),
+                    Width = THUMBNAIL_IMG_WIDTH,
+                    Height = THUMBNAIL_IMG_WIDTH * gallery.files[i].height / gallery.files[i].width,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                SetColumn(_images[i], i);
+                _imageContainer.Children.Add(_images[i]);
+            }
         }
 
         public void EnableButton(bool enable) {
