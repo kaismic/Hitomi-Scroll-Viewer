11c5d020d24dc3371274ef268a64242c7c831b00
Isac Kim <isac08270@gmail.com>
2023-09-18 20:38:15 +1000

    wrote more code

--- Hitomi-Scroll-Viewer/SearchPageComponent/DownloadingItem.xaml.cs
+++ Hitomi-Scroll-Viewer/SearchPageComponent/DownloadingItem.xaml.cs
@@ -6,12 +6,12 @@ using System.Threading;
 
 namespace Hitomi_Scroll_Viewer.SearchPageComponent {
     public sealed partial class DownloadingItem : Grid {
-        private string _statusText = "";
+        private TextBlock _statusText;
         public DownloadingItem(string id) {
             InitializeComponent();
 
             BorderThickness = new(1);
-            Background = new SolidColorBrush(Colors.);
+            Background = new SolidColorBrush(Colors.LightBlue);
             CornerRadius = new(10);
             Padding = new(10);
             ColumnDefinitions.Add(new() { Width = new GridLength(3, GridUnitType.Star) });
@@ -19,21 +19,31 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             ColumnDefinitions.Add(new() { Width = new GridLength(1, GridUnitType.Star) });
             RowDefinitions.Add(new());
             RowDefinitions.Add(new());
-            ColumnSpacing = 4;
+            RowDefinitions.Add(new());
+            ColumnSpacing = 8;
+            RowSpacing = 4;
 
-            TextBlock desc = new() {
+            TextBlock galleryIdText = new() {
                 Text = id
             };
-            SetRow(desc, 0);
-            SetColumn(desc, 0);
-            Children.Add(desc);
+            SetRow(galleryIdText, 0);
+            SetColumn(galleryIdText, 0);
+            Children.Add(galleryIdText);
 
             ProgressBar progressBar = new() {
-                Background = new SolidColorBrush(Colors.Transparent)
+                Background = new SolidColorBrush(Colors.Transparent),
+                Value = 100
             };
             SetRow(progressBar, 1);
             SetColumn(progressBar, 0);
             Children.Add(progressBar);
+
+            _statusText = new() {
+                Text = "Getting Gallery Info..."
+            };
+            SetRow(_statusText, 2);
+            SetColumn(_statusText, 0);
+            Children.Add(_statusText);
 
             Button pauseResumeBtn = new() {
                 Content = new TextBlock() {
@@ -43,7 +53,7 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             };
             SetRow(pauseResumeBtn, 0);
             SetColumn(pauseResumeBtn, 1);
-            SetRowSpan(pauseResumeBtn, 2);
+            SetRowSpan(pauseResumeBtn, 3);
             Children.Add(pauseResumeBtn);
             pauseResumeBtn.Click += PauseResume;
 
@@ -55,7 +65,7 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             };
             SetRow(deleteBtn, 0);
             SetColumn(deleteBtn, 2);
-            SetRowSpan(deleteBtn, 2);
+            SetRowSpan(deleteBtn, 3);
             Children.Add(deleteBtn);
             deleteBtn.Click += Delete;
 
