194c629ff0188c24caa174be1dff62441eebca24
Isac Kim <isac08270@gmail.com>
2024-05-02 17:35:09 +1000

    changed dynamic component initialization to static using xaml

--- Hitomi-Scroll-Viewer/SearchPageComponent/DownloadingItem.xaml.cs
+++ Hitomi-Scroll-Viewer/SearchPageComponent/DownloadingItem.xaml.cs
@@ -1,8 +1,5 @@
-using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -13,7 +10,7 @@ using static Hitomi_Scroll_Viewer.Utils;
 
 namespace Hitomi_Scroll_Viewer.SearchPageComponent {
     public sealed partial class DownloadingItem : Grid {
-        private static readonly string DOWNLOAD_PAUSED = "Download paused";
+        private static readonly string DOWNLOAD_PAUSED = "Download Paused";
         private enum DownloadingState {
             Downloading,
             Paused,
@@ -29,11 +26,6 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
         private Gallery _gallery;
         private readonly string _id;
 
-        private readonly TextBlock _statusText;
-        private readonly ProgressBar _progressBar;
-        private readonly Button _downloadControlBtn;
-        private readonly Button _cancelBtn;
-
         public DownloadingItem(string id, HttpClient httpClient, SearchPage sp, StackPanel parent) {
             _id = id;
             _httpClient = httpClient;
@@ -43,54 +35,9 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
 
             InitializeComponent();
 
-            BorderThickness = new(1);
-            Background = new SolidColorBrush(Colors.LightBlue);
-            CornerRadius = new(10);
-            Padding = new(10);
-            ColumnDefinitions.Add(new() { Width = new GridLength(1, GridUnitType.Star) });
-            ColumnDefinitions.Add(new() { Width = new GridLength(1, GridUnitType.Auto) });
-            ColumnDefinitions.Add(new() { Width = new GridLength(1, GridUnitType.Auto) });
-            RowDefinitions.Add(new());
-            RowDefinitions.Add(new());
-            RowDefinitions.Add(new());
-            ColumnSpacing = 8;
-            RowSpacing = 4;
+            Description.Text += id;
 
-            TextBlock galleryIdText = new() {
-                Text = id
-            };
-            SetRow(galleryIdText, 0);
-            SetColumn(galleryIdText, 0);
-            Children.Add(galleryIdText);
-
-            _progressBar = new() {
-                Background = new SolidColorBrush(Colors.Gray),
-            };
-            SetRow(_progressBar, 1);
-            SetColumn(_progressBar, 0);
-            Children.Add(_progressBar);
-
-            _statusText = new() {
-                TextWrapping = TextWrapping.WrapWholeWords
-            };
-            SetRow(_statusText, 2);
-            SetColumn(_statusText, 0);
-            Children.Add(_statusText);
-
-            _downloadControlBtn = new();
-            SetRow(_downloadControlBtn, 0);
-            SetColumn(_downloadControlBtn, 1);
-            SetRowSpan(_downloadControlBtn, 3);
-            _downloadControlBtn.Click += PauseOrResume;
-
-            _cancelBtn = new() {
-                Content = new SymbolIcon(Symbol.Delete)
-            };
-            ToolTipService.SetToolTip(_cancelBtn, "Cancel Download");
-            SetRow(_cancelBtn, 0);
-            SetColumn(_cancelBtn, 2);
-            SetRowSpan(_cancelBtn, 3);
-            _cancelBtn.Click += (_, _) => {
+            CancelBtn.Click += (_, _) => {
                 EnableButtons(false);
                 _cts.Cancel();
                 if (_gallery != null) {
@@ -99,33 +46,34 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
                 RemoveSelf();
             };
 
-            Children.Add(_downloadControlBtn);
-            Children.Add(_cancelBtn);
-
             Download(_cts.Token);
         }
 
         private void EnableButtons(bool enable) {
-            _downloadControlBtn.IsEnabled = enable;
-            _cancelBtn.IsEnabled = enable;
+            DownloadControlBtn.IsEnabled = enable;
+            CancelBtn.IsEnabled = enable;
         }
 
-        private async void PauseOrResume(object _0, RoutedEventArgs _1) {
+        private void PauseOrResume(object _0, RoutedEventArgs _1) {
             EnableButtons(false);
             switch (_downloadingState) {
                 case DownloadingState.Downloading:
                     // pause
                     _cts.Cancel();
-                    while (_downloadingState == DownloadingState.Downloading) {
-                        await Task.Delay(10);
-                    }
                     break;
                 case DownloadingState.Paused or DownloadingState.Failed:
                     // resume
                     _cts = new();
                     Download(_cts.Token);
+                    EnableButtons(true);
                     break;
             }
+        }
+
+        private void HandleTaskCanceledException() {
+            _downloadingState = DownloadingState.Paused;
+            DownloadStatus.Text = DOWNLOAD_PAUSED;
+            SetDownloadControlBtnState();
             EnableButtons(true);
         }
 
@@ -133,51 +81,48 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             _downloadingState = DownloadingState.Downloading;
             SetDownloadControlBtnState();
             if (_gallery == null) {
-                _statusText.Text = "Getting gallery info...";
+                DownloadStatus.Text = "Getting gallery info...";
                 string galleryInfo;
                 try {
                     galleryInfo = await GetGalleryInfo(_httpClient, _id, ct);
                 } catch (HttpRequestException e) {
                     _downloadingState = DownloadingState.Failed;
-                    _statusText.Text = "An error has occurred while getting gallery info.\n" + e.Message;
+                    DownloadStatus.Text = "An error has occurred while getting gallery info.\n" + e.Message;
                     if (e.InnerException != null) {
-                        _statusText.Text += "\n" + e.InnerException.Message;
+                        DownloadStatus.Text += "\n" + e.InnerException.Message;
                     }
                     SetDownloadControlBtnState();
                     return;
                 } catch (TaskCanceledException) {
-                    _downloadingState = DownloadingState.Paused;
-                    _statusText.Text = DOWNLOAD_PAUSED;
-                    SetDownloadControlBtnState();
+                    HandleTaskCanceledException();
                     return;
                 }
 
-                _statusText.Text = "Reading gallery JSON file...";
+                DownloadStatus.Text = "Reading gallery JSON file...";
                 try {
                     _gallery = JsonSerializer.Deserialize<Gallery>(galleryInfo, serializerOptions);
-                    _progressBar.Maximum = _gallery.files.Length;
+                    DownloadProgressBar.Maximum = _gallery.files.Length;
+                    Description.Text += $" - {_gallery.title}"; // add title to description
                 }
                 catch (JsonException e) {
                     _downloadingState = DownloadingState.Failed;
-                    _statusText.Text = "An error has occurred while reading gallery json.\n" + e.Message;
+                    DownloadStatus.Text = "An error has occurred while reading gallery json.\n" + e.Message;
                     SetDownloadControlBtnState();
                     return;
                 }
             }
 
-            _statusText.Text = "Getting server time...";
+            DownloadStatus.Text = "Getting server time...";
             string serverTime;
             try {
                 serverTime = await GetServerTime(_httpClient, ct);
             } catch (HttpRequestException e) {
                 _downloadingState = DownloadingState.Failed;
-                _statusText.Text = "An error has occurred while getting the server time.\n" + e.Message;
+                DownloadStatus.Text = "An error has occurred while getting the server time.\n" + e.Message;
                 SetDownloadControlBtnState();
                 return;
             } catch (TaskCanceledException) {
-                _downloadingState = DownloadingState.Paused;
-                _statusText.Text = DOWNLOAD_PAUSED;
-                SetDownloadControlBtnState();
+                HandleTaskCanceledException();
                 return;
             }
 
@@ -193,9 +138,9 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
             } catch (DirectoryNotFoundException) {
                 missingIndexes = Enumerable.Range(0, _gallery.files.Length).ToArray();
             }
-            _progressBar.Value = _gallery.files.Length - missingIndexes.Length;
+            DownloadProgressBar.Value = _gallery.files.Length - missingIndexes.Length;
 
-            _statusText.Text = "Getting Image Addresses...";
+            DownloadStatus.Text = "Getting Image Addresses...";
             ImageInfo[] imageInfos = new ImageInfo[missingIndexes.Length];
             for (int i = 0; i < missingIndexes.Length; i++) {
                 imageInfos[i] = _gallery.files[missingIndexes[i]];
@@ -205,12 +150,12 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
 
             if (ct.IsCancellationRequested) {
                 _downloadingState = DownloadingState.Paused;
-                _statusText.Text = DOWNLOAD_PAUSED;
+                DownloadStatus.Text = DOWNLOAD_PAUSED;
                 SetDownloadControlBtnState();
                 return;
             }
 
-            _statusText.Text = "Downloading Images...";
+            DownloadStatus.Text = "Downloading Images...";
             Task[] tasks = DownloadImages(
                 _httpClient,
                 _id,
@@ -218,16 +163,14 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
                 imgFormats,
                 missingIndexes,
                 _sp.DownloadThreadNum,
-                _progressBar,
+                DownloadProgressBar,
                 ct
             );
             Task allTask = Task.WhenAll(tasks);
             try {
                 await allTask;
             } catch (TaskCanceledException) {
-                _downloadingState = DownloadingState.Paused;
-                _statusText.Text = DOWNLOAD_PAUSED;
-                SetDownloadControlBtnState();
+                HandleTaskCanceledException();
                 return;
             }
             
@@ -236,7 +179,7 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
                 missingIndexes = GetMissingIndexes(_gallery);
                 if (missingIndexes.Length > 0) {
                     _downloadingState = DownloadingState.Failed;
-                    _statusText.Text = $"Failed to download {missingIndexes.Length} images";
+                    DownloadStatus.Text = $"Failed to download {missingIndexes.Length} images";
                     SetDownloadControlBtnState();
                 } else {
                     EnableButtons(false);
@@ -244,7 +187,7 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
                 }
             } else {
                 _downloadingState = DownloadingState.Failed;
-                _statusText.Text = "An unknown error has occurred. Please try again";
+                DownloadStatus.Text = "An unknown error has occurred. Please try again";
                 SetDownloadControlBtnState();
             }
         }
@@ -252,17 +195,16 @@ namespace Hitomi_Scroll_Viewer.SearchPageComponent {
         private void SetDownloadControlBtnState() {
             switch (_downloadingState) {
                 case DownloadingState.Downloading:
-                    _downloadControlBtn.Content = new SymbolIcon(Symbol.Pause);
-                    ToolTipService.SetToolTip(_downloadControlBtn, "Pause Download");
+                    DownloadControlBtn.Content = new SymbolIcon(Symbol.Pause);
+                    ToolTipService.SetToolTip(DownloadControlBtn, "Pause Download");
                     break;
                 case DownloadingState.Paused:
-                    _downloadControlBtn.Content = new SymbolIcon(Symbol.Play);
-                    ToolTipService.SetToolTip(_downloadControlBtn, "Resume Download");
-                    
+                    DownloadControlBtn.Content = new SymbolIcon(Symbol.Play);
+                    ToolTipService.SetToolTip(DownloadControlBtn, "Resume Download");
                     break;
                 case DownloadingState.Failed:
-                    _downloadControlBtn.Content = new SymbolIcon(Symbol.Refresh);
-                    ToolTipService.SetToolTip(_downloadControlBtn, "Try Again");
+                    DownloadControlBtn.Content = new SymbolIcon(Symbol.Refresh);
+                    ToolTipService.SetToolTip(DownloadControlBtn, "Try Again");
                     break;
             }
         }
