a07ff01dcf795bf531b617947f7009622b9557fc
Isac Kim <isac08270@gmail.com>
2024-05-14 17:32:59 +1000

    created ItemsChangeObservableCollection

---
+++ Hitomi-Scroll-Viewer/ItemsChangeObservableCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Hitomi_Scroll_Viewer {
+    internal class ItemsChangeObservableCollection<T> : ObservableCollection<T> {
+        public void NotifyItemChange() {
+            base.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
