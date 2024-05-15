3d7201855e1ab2bc6f2b947f3d6cad361619e04c
Isac Kim <isac08270@gmail.com>
2024-05-16 01:03:29 +1000

    renamed to SearchTag

---
+++ Hitomi-Scroll-Viewer/SearchTag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Hitomi_Scroll_Viewer {
+    public class SearchTag {
+        public static readonly string[] CATEGORIES = [
+            "language", "female", "male", "artist", "character", "group", "series", "type", "tag"
+        ];
+
+        public Dictionary<string, string[]> includeTags = [];
+        public Dictionary<string, string[]> excludeTags = [];
+        public SearchTag() {
+            foreach (string tag in CATEGORIES) {
+                includeTags[tag] = [];
+                excludeTags[tag] = [];
+            }
+        }
+    }
+}
