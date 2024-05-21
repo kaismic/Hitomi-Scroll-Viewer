f70e56e2687841c5de7d2d13d8ab848a91f9317a
Isac Kim <isac08270@gmail.com>
2024-05-22 02:10:05 +1000

    created SearchLinkItem

---
+++ Hitomi-Scroll-Viewer/SearchPageComponent/SearchLinkItem.xaml.cs
@@ -0,0 +1,20 @@
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Windows.Foundation;
+
+namespace Hitomi_Scroll_Viewer.SearchPageComponent {
+    public sealed partial class SearchLinkItem : Grid {
+        private readonly string _searchLink;
+        private readonly string _displayText;
+        public SearchLinkItem(
+            string searchLink,
+            string displayText,
+            TypedEventHandler<XamlUICommand, ExecuteRequestedEventArgs> handler
+            ) {
+            _searchLink = searchLink;
+            _displayText = displayText;
+            InitializeComponent();
+            DeleteCommand.ExecuteRequested += handler;
+        }
+    }
+}
