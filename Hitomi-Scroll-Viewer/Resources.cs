3d983f252b9ec5b2db38677678cc9479213e2c58
Isac Kim <isac08270@gmail.com>
2024-06-02 22:41:47 +1000

    created Resources.cs

---
+++ Hitomi-Scroll-Viewer/Resources.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Windows.ApplicationModel.Resources;
+
+namespace Hitomi_Scroll_Viewer {
+    internal class Resources {
+        private static readonly ResourceMap ResourceManager = new ResourceManager().MainResourceMap.GetSubtree("Resources");
+        internal static readonly string DIALOG_TEXT_YES = ResourceManager.GetValue("DialogText_Yes").ValueAsString;
+        internal static readonly string DIALOG_TEXT_CANCEL = ResourceManager.GetValue("DialogText_Cancel").ValueAsString;
+    }
+}
