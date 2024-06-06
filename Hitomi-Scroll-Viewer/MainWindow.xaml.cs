83c6f12c30f75b30af0db3d711ac71789f1e5400
Isac Kim <isac08270@gmail.com>
2024-06-06 21:15:49 +1000

    fixed MainWindow title setting

--- Hitomi-Scroll-Viewer/MainWindow.xaml.cs
+++ Hitomi-Scroll-Viewer/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Hitomi_Scroll_Viewer {
         public MainWindow() {
             ((OverlappedPresenter)AppWindow.Presenter).Maximize();
             InitializeComponent();
-            Title = MainResourceMap.GetValue("AppDisplayName").ValueAsString;
+            Title = MainResourceMap.GetSubtree("Resources").GetValue("AppDisplayName").ValueAsString;
             // create directories if they don't exist
             Directory.CreateDirectory(ROOT_DIR);
             Directory.CreateDirectory(IMAGE_DIR);
