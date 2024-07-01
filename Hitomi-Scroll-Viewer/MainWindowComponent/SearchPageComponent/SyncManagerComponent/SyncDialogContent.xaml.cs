b9966be8350b0aa9f1012e58f86c647d13f5514a
Isac Kim <isac08270@gmail.com>
2024-07-01 21:11:17 +1000

    created SyncDialogContent

---
+++ Hitomi-Scroll-Viewer/MainWindowComponent/SearchPageComponent/SyncManagerComponent/SyncDialogContent.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Hitomi_Scroll_Viewer.MainWindowComponent.SearchPageComponent.SyncManagerComponent {
+    public sealed partial class SyncDialogContent : Grid {
+        public SyncDialogContent() {
+            InitializeComponent();
+        }
+    }
+}
