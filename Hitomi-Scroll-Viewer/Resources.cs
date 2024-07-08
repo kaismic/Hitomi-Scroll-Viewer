349f89d5ba840b8be8c378331eda3bf245379b06
Isac Kim <isac08270@gmail.com>
2024-07-08 20:23:46 +1000

    added Text_Error

--- Hitomi-Scroll-Viewer/Resources.cs
+++ Hitomi-Scroll-Viewer/Resources.cs
@@ -3,19 +3,20 @@
 namespace Hitomi_Scroll_Viewer {
     internal class Resources {
         internal static readonly ResourceMap MainResourceMap = new ResourceManager().MainResourceMap;
-        private static readonly ResourceMap ResourceMap = MainResourceMap.GetSubtree("Resources");
+        private static readonly ResourceMap _resourceMap = MainResourceMap.GetSubtree("Resources");
 
-        internal static readonly string APP_DISPLAY_NAME = ResourceMap.GetValue("AppDisplayName").ValueAsString;
+        internal static readonly string APP_DISPLAY_NAME = _resourceMap.GetValue("AppDisplayName").ValueAsString;
         
-        internal static readonly string TEXT_YES = ResourceMap.GetValue("Text_Yes").ValueAsString;
-        internal static readonly string TEXT_NO = ResourceMap.GetValue("Text_No").ValueAsString;
-        internal static readonly string TEXT_CANCEL = ResourceMap.GetValue("Text_Cancel").ValueAsString;
-        internal static readonly string TEXT_EXIT = ResourceMap.GetValue("Text_Exit").ValueAsString;
-        internal static readonly string TEXT_CLOSE = ResourceMap.GetValue("Text_Close").ValueAsString;
+        internal static readonly string TEXT_YES = _resourceMap.GetValue("Text_Yes").ValueAsString;
+        internal static readonly string TEXT_NO = _resourceMap.GetValue("Text_No").ValueAsString;
+        internal static readonly string TEXT_CANCEL = _resourceMap.GetValue("Text_Cancel").ValueAsString;
+        internal static readonly string TEXT_EXIT = _resourceMap.GetValue("Text_Exit").ValueAsString;
+        internal static readonly string TEXT_CLOSE = _resourceMap.GetValue("Text_Close").ValueAsString;
+        internal static readonly string TEXT_ERROR = _resourceMap.GetValue("Text_Error").ValueAsString;
 
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_1 = ResourceMap.GetValue("ExampleTagFilterName_1").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_2 = ResourceMap.GetValue("ExampleTagFilterName_2").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_3 = ResourceMap.GetValue("ExampleTagFilterName_3").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_4 = ResourceMap.GetValue("ExampleTagFilterName_4").ValueAsString;
+        internal static readonly string EXAMPLE_TAG_FILTER_NAME_1 = _resourceMap.GetValue("ExampleTagFilterName_1").ValueAsString;
+        internal static readonly string EXAMPLE_TAG_FILTER_NAME_2 = _resourceMap.GetValue("ExampleTagFilterName_2").ValueAsString;
+        internal static readonly string EXAMPLE_TAG_FILTER_NAME_3 = _resourceMap.GetValue("ExampleTagFilterName_3").ValueAsString;
+        internal static readonly string EXAMPLE_TAG_FILTER_NAME_4 = _resourceMap.GetValue("ExampleTagFilterName_4").ValueAsString;
     }
 }
