5232c86ee529db987b3f1a0d1b82e1f9569a9131
Isac Kim <isac08270@gmail.com>
2024-07-30 22:42:07 +1000

    removed TagFilterSetName

--- Hitomi-Scroll-Viewer/Entities/TagFilter.cs
+++ Hitomi-Scroll-Viewer/Entities/TagFilter.cs
@@ -9,8 +9,6 @@ namespace Hitomi_Scroll_Viewer.Entities
             "language", "female", "male", "artist", "character", "group", "series", "type", "tag"
         ];
 
-        [MaxLength(TagFilterSet.TAG_FILTER_SET_NAME_MAX_LEN)]
-        public string TagFilterSetName { get; set; }
         [MaxLength(9)]
         public string Category { get; set; }
         public IEnumerable<string> TagFilters { get; set; }
