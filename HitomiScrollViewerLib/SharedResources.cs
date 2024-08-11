610ca74f7dc446bd70351c34872fd05dead46f3e
Isac Kim <isac08270@gmail.com>
2024-08-11 17:19:39 +1000

    made public and modified to fit select HitomiScrollViewerLib

--- HitomiScrollViewerLib/SharedResources.cs
+++ HitomiScrollViewerLib/SharedResources.cs
@@ -1,25 +1,25 @@
 ﻿using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace HitomiScrollViewerLib {
-    internal class SharedResources {
-        internal static readonly ResourceMap MainResourceMap = new ResourceManager().MainResourceMap;
-        private static readonly ResourceMap _resourceMap = MainResourceMap.GetSubtree("Resources");
+    public class SharedResources {
+        public static readonly ResourceMap MainResourceMap = new ResourceManager().MainResourceMap;
+        private static readonly ResourceMap _resourceMap = MainResourceMap.GetSubtree("HitomiScrollViewerLib/SharedResources");
 
-        internal static readonly string APP_DISPLAY_NAME = _resourceMap.GetValue("AppDisplayName").ValueAsString;
+        public static readonly string APP_DISPLAY_NAME = _resourceMap.GetValue("AppDisplayName").ValueAsString;
         
-        internal static readonly string TEXT_YES = _resourceMap.GetValue("Text_Yes").ValueAsString;
-        internal static readonly string TEXT_NO = _resourceMap.GetValue("Text_No").ValueAsString;
-        internal static readonly string TEXT_CANCEL = _resourceMap.GetValue("Text_Cancel").ValueAsString;
-        internal static readonly string TEXT_EXIT = _resourceMap.GetValue("Text_Exit").ValueAsString;
-        internal static readonly string TEXT_CLOSE = _resourceMap.GetValue("Text_Close").ValueAsString;
-        internal static readonly string TEXT_ERROR = _resourceMap.GetValue("Text_Error").ValueAsString;
-        internal static readonly string TEXT_TAG_FILTERS = _resourceMap.GetValue("Text_TagFilters").ValueAsString;
-        internal static readonly string TEXT_BOOKMARKS = _resourceMap.GetValue("Text_Bookmarks").ValueAsString;
-        internal static readonly string TEXT_PAGE = _resourceMap.GetValue("Text_Page").ValueAsString;
+        public static readonly string TEXT_YES = _resourceMap.GetValue("Text_Yes").ValueAsString;
+        public static readonly string TEXT_NO = _resourceMap.GetValue("Text_No").ValueAsString;
+        public static readonly string TEXT_CANCEL = _resourceMap.GetValue("Text_Cancel").ValueAsString;
+        public static readonly string TEXT_EXIT = _resourceMap.GetValue("Text_Exit").ValueAsString;
+        public static readonly string TEXT_CLOSE = _resourceMap.GetValue("Text_Close").ValueAsString;
+        public static readonly string TEXT_ERROR = _resourceMap.GetValue("Text_Error").ValueAsString;
+        public static readonly string TEXT_TAG_FILTERS = _resourceMap.GetValue("Text_TagFilters").ValueAsString;
+        public static readonly string TEXT_BOOKMARKS = _resourceMap.GetValue("Text_Bookmarks").ValueAsString;
+        public static readonly string TEXT_PAGE = _resourceMap.GetValue("Text_Page").ValueAsString;
 
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_1 = _resourceMap.GetValue("ExampleTagFilterSet_1").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_2 = _resourceMap.GetValue("ExampleTagFilterSet_2").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_3 = _resourceMap.GetValue("ExampleTagFilterSet_3").ValueAsString;
-        internal static readonly string EXAMPLE_TAG_FILTER_NAME_4 = _resourceMap.GetValue("ExampleTagFilterSet_4").ValueAsString;
+        public static readonly string EXAMPLE_TAG_FILTER_NAME_1 = _resourceMap.GetValue("ExampleTagFilterSet_1").ValueAsString;
+        public static readonly string EXAMPLE_TAG_FILTER_NAME_2 = _resourceMap.GetValue("ExampleTagFilterSet_2").ValueAsString;
+        public static readonly string EXAMPLE_TAG_FILTER_NAME_3 = _resourceMap.GetValue("ExampleTagFilterSet_3").ValueAsString;
+        public static readonly string EXAMPLE_TAG_FILTER_NAME_4 = _resourceMap.GetValue("ExampleTagFilterSet_4").ValueAsString;
     }
 }
