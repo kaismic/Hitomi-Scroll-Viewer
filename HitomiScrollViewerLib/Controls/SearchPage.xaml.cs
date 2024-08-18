566c685c663b83c0e46e99d6460bf15b9533ee3a
Isac Kim <isac08270@gmail.com>
2024-08-18 23:21:48 +1000

    removed unused using

--- HitomiScrollViewerLib/Controls/SearchPage.xaml.cs
+++ HitomiScrollViewerLib/Controls/SearchPage.xaml.cs
@@ -16,7 +16,6 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
 using static HitomiScrollViewerLib.SharedResources;
