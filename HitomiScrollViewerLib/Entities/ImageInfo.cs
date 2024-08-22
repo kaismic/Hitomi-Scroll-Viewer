4ca6f75a7e46023ceef92cae1a74f3c42cc382ec
Isac Kim <isac08270@gmail.com>
2024-08-22 23:20:04 +1000

    added Index prop and Clone() method

--- HitomiScrollViewerLib/Entities/ImageInfo.cs
+++ HitomiScrollViewerLib/Entities/ImageInfo.cs
@@ -1,9 +1,12 @@
-﻿using System.Text.Json.Serialization;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace HitomiScrollViewerLib.Entities {
+    [Index(nameof(Index))]
     public class ImageInfo {
         [JsonIgnore]
         public long Id { get; set; }
+        public int Index { get; set; }
         public string Name { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
@@ -13,5 +16,9 @@ namespace HitomiScrollViewerLib.Entities {
         public int Hasjxl { get; set; }
         public string Hash { get; set; }
         public virtual Gallery Gallery { get; set; }
+
+        public ImageInfo Clone() {
+            return MemberwiseClone() as ImageInfo;
+        }
     }
 }
