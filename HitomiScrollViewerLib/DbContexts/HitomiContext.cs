b9ad8ff298f22957c5705da810dbbf85532c48b1
Isac Kim <isac08270@gmail.com>
2024-08-24 23:04:23 +1000

    fixed example tag values

--- HitomiScrollViewerLib/DbContexts/HitomiContext.cs
+++ HitomiScrollViewerLib/DbContexts/HitomiContext.cs
@@ -18,7 +18,7 @@ namespace HitomiScrollViewerLib.DbContexts {
             get => _main ??= new HitomiContext();
             set => _main = value;
         }
-
+                
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             // db file storage location = Windows.Storage.ApplicationData.Current.LocalFolder.Path
             optionsBuilder.UseSqlite($"Data Source={MAIN_DATABASE_PATH_V3}");
@@ -84,28 +84,28 @@ namespace HitomiScrollViewerLib.DbContexts {
                 new() {
                     Name = EXAMPLE_TAG_FILTER_SET_1,
                     Tags = [
-                        Tag.GetTag("full_color", Category.Tag),
-                        Tag.GetTag("very_long_hair", Category.Female),
+                        Tag.GetTag("full color", Category.Tag),
+                        Tag.GetTag("very long hair", Category.Female),
                     ]
                 },
                 new() {
                     Name = EXAMPLE_TAG_FILTER_SET_2,
                     Tags = [
                         Tag.GetTag("glasses", Category.Female),
-                        Tag.GetTag("sole_male", Category.Male),
+                        Tag.GetTag("sole male", Category.Male),
                     ]
                 },
                 new() {
                     Name = EXAMPLE_TAG_FILTER_SET_3,
                     Tags = [
                         Tag.GetTag("naruto", Category.Series),
-                        Tag.GetTag("big_breasts", Category.Tag),
+                        Tag.GetTag("big breasts", Category.Tag),
                     ]
                 },
                 new() {
                     Name = EXAMPLE_TAG_FILTER_SET_4,
                     Tags = [
-                        Tag.GetTag("non-h_imageset", Category.Tag)
+                        Tag.GetTag("non-h imageset", Category.Tag)
                     ]
                 }
             );
