aaf18cac18a7338e5d7be4e37950ffe2d122616e
Isac Kim <isac08270@gmail.com>
2024-08-26 15:44:31 +1000

    created GalleryLanguage

---
+++ HitomiScrollViewerLib/Entities/GalleryLanguage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HitomiScrollViewerLib.Entities {
+    [Index(nameof(DisplayName))]
+    public class GalleryLanguage {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string SearchParamValue { get; set; }
+    }
+}
