9c8c290fa6147fbd551bdfb6a8e9bf53f46ae124
Isac Kim <isac08270@gmail.com>
2024-08-29 22:02:41 +1000

    implemented SearchLinkItem

--- HitomiScrollViewerLib/Views/SearchPage/SearchLinkItem.xaml.cs
+++ HitomiScrollViewerLib/Views/SearchPage/SearchLinkItem.xaml.cs
@@ -2,17 +2,17 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Windows.Foundation;
 
-namespace HitomiScrollViewerLib.Controls.SearchPageComponents {
+namespace HitomiScrollViewerLib.Views.SearchPage {
     public sealed partial class SearchLinkItem : Grid {
-        internal readonly string SearchLink;
-        private readonly string _displayText;
+        public readonly string SearchLink;
+        private string DisplayText { get; }
         public SearchLinkItem(
             string searchLink,
             string displayText,
             TypedEventHandler<XamlUICommand, ExecuteRequestedEventArgs> handler
         ) {
             SearchLink = searchLink;
-            _displayText = displayText;
+            DisplayText = displayText;
             InitializeComponent();
             DeleteCommand.ExecuteRequested += handler;
         }
