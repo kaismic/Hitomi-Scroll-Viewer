44617948a19e9850a9b0c451d3c458440f1f166c
Isac Kim <isac08270@gmail.com>
2024-09-16 19:01:06 +1000

    added ImagesPerPage

--- HitomiScrollViewerLib/Models/CommonSettings.cs
+++ HitomiScrollViewerLib/Models/CommonSettings.cs
@@ -2,6 +2,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System.Collections.Generic;
+using System.Linq;
 using Windows.Storage;
 
 namespace HitomiScrollViewerLib.Models {
@@ -14,6 +15,10 @@ namespace HitomiScrollViewerLib.Models {
             new() { Value = Orientation.Vertical },
             new() { Value = Orientation.Horizontal }
         ];
+        public static readonly List<string> IMAGES_PER_PAGE_ITEMS = [
+            "Auto (Recommended)",
+            .. Enumerable.Range(1, 5).Select(x => x.ToString())
+        ];
 
         [ObservableProperty]
         private bool _isTFAutoSaveEnabled = (bool)(ApplicationData.Current.LocalSettings.Values[nameof(IsTFAutoSaveEnabled)] ??= true);
@@ -23,6 +28,8 @@ namespace HitomiScrollViewerLib.Models {
         private bool _isPageFlipEffectEnabled = (bool)(ApplicationData.Current.LocalSettings.Values[nameof(IsPageFlipEffectEnabled)] ??= true);
         [ObservableProperty]
         private ScrollDirection _scrollDirection = SCROLL_DIRECTIONS.Find(sd => sd.Value == (Orientation)(ApplicationData.Current.LocalSettings.Values[nameof(ScrollDirection)] ??= Orientation.Vertical));
+        [ObservableProperty]
+        private int _imagesPerPage = (int)(ApplicationData.Current.LocalSettings.Values[nameof(ImagesPerPage)] ??= 0);
 
         partial void OnIsTFAutoSaveEnabledChanged(bool value) {
             ApplicationData.Current.LocalSettings.Values[nameof(IsTFAutoSaveEnabled)] = value;
@@ -36,9 +43,12 @@ namespace HitomiScrollViewerLib.Models {
         partial void OnScrollDirectionChanged(ScrollDirection value) {
             ApplicationData.Current.LocalSettings.Values[nameof(ScrollDirection)] = value.Value;
         }
+        partial void OnImagesPerPageChanged(int value) {
+            ApplicationData.Current.LocalSettings.Values[nameof(ImagesPerPage)] = value;
+        }
 
         private static CommonSettings _main;
         public static CommonSettings Main => _main ??= new();
-        private CommonSettings() { }
+        private CommonSettings() {}
     }
 }
