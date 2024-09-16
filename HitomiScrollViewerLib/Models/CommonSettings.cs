10946f85a6689c4c6f9dee68bef58f4f35f2b7d9
Isac Kim <isac08270@gmail.com>
2024-09-16 17:36:03 +1000

    completed implementing CommonSettings and SettingsPage

--- HitomiScrollViewerLib/Models/CommonSettings.cs
+++ HitomiScrollViewerLib/Models/CommonSettings.cs
@@ -1,12 +1,40 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
 using Windows.Storage;
 
 namespace HitomiScrollViewerLib.Models {
     public partial class CommonSettings : ObservableObject {
+        public static readonly List<FlowDirectionModel> FLOW_DIRECTION_MODELS = [
+            new() { Value = FlowDirection.LeftToRight },
+            new() { Value = FlowDirection.RightToLeft }
+        ];
+        public static readonly List<ScrollDirection> SCROLL_DIRECTIONS = [
+            new() { Value = Orientation.Vertical },
+            new() { Value = Orientation.Horizontal }
+        ];
+
         [ObservableProperty]
         private bool _isTFAutoSaveEnabled = (bool)(ApplicationData.Current.LocalSettings.Values[nameof(IsTFAutoSaveEnabled)] ??= true);
+        [ObservableProperty]
+        private FlowDirectionModel _flowDirectionModel = FLOW_DIRECTION_MODELS.Find(fd => fd.Value == (FlowDirection)(ApplicationData.Current.LocalSettings.Values[nameof(FlowDirection)] ??= FlowDirection.RightToLeft));
+        [ObservableProperty]
+        private bool _isPageFlipEffectEnabled = (bool)(ApplicationData.Current.LocalSettings.Values[nameof(IsPageFlipEffectEnabled)] ??= true);
+        [ObservableProperty]
+        private ScrollDirection _scrollDirection = SCROLL_DIRECTIONS.Find(sd => sd.Value == (Orientation)(ApplicationData.Current.LocalSettings.Values[nameof(ScrollDirection)] ??= Orientation.Vertical));
+
         partial void OnIsTFAutoSaveEnabledChanged(bool value) {
             ApplicationData.Current.LocalSettings.Values[nameof(IsTFAutoSaveEnabled)] = value;
+        }
+        partial void OnFlowDirectionModelChanged(FlowDirectionModel value) {
+            ApplicationData.Current.LocalSettings.Values[nameof(FlowDirectionModel)] = value.Value;
+        }
+        partial void OnIsPageFlipEffectEnabledChanged(bool value) {
+            ApplicationData.Current.LocalSettings.Values[nameof(IsPageFlipEffectEnabled)] = value;
+        }
+        partial void OnScrollDirectionChanged(ScrollDirection value) {
+            ApplicationData.Current.LocalSettings.Values[nameof(ScrollDirection)] = value.Value;
         }
 
         private static CommonSettings _main;
--- HitomiScrollViewerLib/Views/PageViews/SettingsPage.xaml.cs
+++ HitomiScrollViewerLib/Views/PageViews/SettingsPage.xaml.cs
@@ -1,28 +1,12 @@
-using Microsoft.UI.Xaml;
+using HitomiScrollViewerLib.Models;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace HitomiScrollViewerLib.Views.PageViews {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page {
+        public CommonSettings CommonSettings { get; } = CommonSettings.Main;
+
         public SettingsPage() {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
