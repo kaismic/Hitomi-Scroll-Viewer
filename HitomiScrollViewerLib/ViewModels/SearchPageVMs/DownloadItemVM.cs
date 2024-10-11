71bef666174e53874557db8565d07e1537096b2d
Isac Kim <isac08270@gmail.com>
2024-10-11 17:12:38 +1100

    removed unused import

--- HitomiScrollViewerLib/ViewModels/SearchPageVMs/DownloadItemVM.cs
+++ HitomiScrollViewerLib/ViewModels/SearchPageVMs/DownloadItemVM.cs
@@ -16,7 +16,6 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.Foundation;
 using static HitomiScrollViewerLib.Constants;
 using static HitomiScrollViewerLib.SharedResources;
 
