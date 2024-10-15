63b7da7b17874334dd0a73da2b7426492a6f7cee
Isac Kim <isac08270@gmail.com>
2024-10-15 17:11:25 +1100

    removed unused imports

--- HitomiScrollViewerLib/Views/PageViews/BrowsePage.xaml.cs
+++ HitomiScrollViewerLib/Views/PageViews/BrowsePage.xaml.cs
@@ -5,9 +5,6 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace HitomiScrollViewerLib.Views.PageViews {
