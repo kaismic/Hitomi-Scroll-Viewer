fe3c42f6afb92229c4b6ffb443b3f31d75b027f9
Isac Kim <isac08270@gmail.com>
2024-11-08 14:14:37 +1100

    the flipview bug was actually not a bug

--- HitomiScrollViewerLib/Views/ViewPageViews/GalleryTabViewItem.xaml.cs
+++ HitomiScrollViewerLib/Views/ViewPageViews/GalleryTabViewItem.xaml.cs
@@ -1,11 +1,8 @@
-using HitomiScrollViewerLib.Models;
 using HitomiScrollViewerLib.ViewModels.ViewPageVMs;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.Web.WebView2.Core;
-using System;
 using System.Linq;
 
 namespace HitomiScrollViewerLib.Views.ViewPageViews {
@@ -70,44 +67,6 @@ namespace HitomiScrollViewerLib.Views.ViewPageViews {
                 ActionIcon2.Glyph = glyph2;
             }
             FadeOutStoryboard.Begin();
-        }
-
-        private static readonly string VIRTUAL_HOST_NAME = "images";
-        // {0} = full file name
-        // {1} = invert y-axis css
-        private static readonly string IMAGE_HTML =
-            """<body style="margin: 0;"><img src="http://""" +
-            VIRTUAL_HOST_NAME +
-            """/{0}" style="width:100%; height:100%;{1}"></img></body>""";
-        // CommonSettings.FlowDirection affects WebView2 so gotta invert the images on y-axis
-        private const string INVERT_Y_AXIS_STYLE_CSS = "transform: scaleX(-1);";
-
-
-        private void WebView2_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs _1) {
-            sender.CoreWebView2.SetVirtualHostNameToFolderMapping(VIRTUAL_HOST_NAME, ViewModel.NonVirtualImageDirPath, CoreWebView2HostResourceAccessKind.DenyCors);
-            NavigateToImageHtml(sender);
-        }
-
-        private void WebView2_Loaded(object sender, RoutedEventArgs e) {
-            var webView2 = sender as WebView2;
-            SizeAdjustedImageInfo info = webView2.Tag as SizeAdjustedImageInfo;
-            _ = webView2.EnsureCoreWebView2Async();
-            if (info.LastFlowDirection != CommonSettings.Main.FlowDirectionModel.Value) {
-                NavigateToImageHtml(webView2);
-            }
-        }
-
-        private static void NavigateToImageHtml(WebView2 webView2) {
-            SizeAdjustedImageInfo info = webView2.Tag as SizeAdjustedImageInfo;
-            FlowDirection fd = CommonSettings.Main.FlowDirectionModel.Value;
-            info.LastFlowDirection = fd;
-            webView2.NavigateToString(
-                string.Format(
-                    IMAGE_HTML,
-                    info.FullFileName,
-                    fd == FlowDirection.LeftToRight ? "" : INVERT_Y_AXIS_STYLE_CSS
-                )
-            );
         }
 
         private void FlipView_PreviewKeyDown(object _0, KeyRoutedEventArgs e) {
--- HitomiScrollViewerLib/Views/ViewPageViews/ImageCollectionPanel.xaml.cs
+++ HitomiScrollViewerLib/Views/ViewPageViews/ImageCollectionPanel.xaml.cs
@@ -1,25 +1,64 @@
-using HitomiScrollViewerLib.Entities;
+using HitomiScrollViewerLib.Models;
 using HitomiScrollViewerLib.ViewModels.ViewPageVMs;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
 
 namespace HitomiScrollViewerLib.Views.ViewPageViews {
     public sealed partial class ImageCollectionPanel : UserControl {
-        private static readonly string VIRTUAL_HOST_NAME = "images.example";
-        // {0} = full file name
-        private static readonly string IMAGE_HTML =
-            """<body style="margin: 0;"><img src="http://""" +
-            VIRTUAL_HOST_NAME +
-            """/{0}" style="width:100%; height:100%;"/></body>""";
-        public ImageCollectionPanelVM ViewModel { get; set; }
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            nameof(ViewModel),
+            typeof(ImageCollectionPanelVM),
+            typeof(ImageCollectionPanel),
+            null
+        );
+
+        public ImageCollectionPanelVM ViewModel {
+            get => (ImageCollectionPanelVM)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
 
         public ImageCollectionPanel() {
             InitializeComponent();
         }
 
+        private static readonly string VIRTUAL_HOST_NAME = "images";
+        // {0} = full file name
+        // {1} = invert y-axis css
+        private static readonly string IMAGE_HTML =
+            """<body style="margin: 0;"><img src="http://""" +
+            VIRTUAL_HOST_NAME +
+            """/{0}" style="width:100%; height:100%;{1}"></img></body>""";
+        // CommonSettings.FlowDirection affects WebView2 so gotta invert the images on y-axis
+        private const string INVERT_Y_AXIS_STYLE_CSS = "transform: scaleX(-1);";
+
+
         private void WebView2_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs _1) {
             sender.CoreWebView2.SetVirtualHostNameToFolderMapping(VIRTUAL_HOST_NAME, ViewModel.NonVirtualImageDirPath, CoreWebView2HostResourceAccessKind.DenyCors);
-            sender.NavigateToString(string.Format(IMAGE_HTML, (sender.Tag as ImageInfo).FullFileName));
+            NavigateToImageHtml(sender);
         }
+
+        private void WebView2_Loaded(object sender, RoutedEventArgs e) {
+            var webView2 = sender as WebView2;
+            SizeAdjustedImageInfo info = webView2.Tag as SizeAdjustedImageInfo;
+            _ = webView2.EnsureCoreWebView2Async();
+            if (info.LastFlowDirection != CommonSettings.Main.FlowDirectionModel.Value) {
+                NavigateToImageHtml(webView2);
+            }
+        }
+
+        private static void NavigateToImageHtml(WebView2 webView2) {
+            SizeAdjustedImageInfo info = webView2.Tag as SizeAdjustedImageInfo;
+            FlowDirection fd = CommonSettings.Main.FlowDirectionModel.Value;
+            info.LastFlowDirection = fd;
+            webView2.NavigateToString(
+                string.Format(
+                    IMAGE_HTML,
+                    info.FullFileName,
+                    fd == FlowDirection.LeftToRight ? "" : INVERT_Y_AXIS_STYLE_CSS
+                )
+            );
+        }
+
     }
 }
