a1f0b73de6d950a4b3090e974a066b7ad154230c
Isac Kim <isac08270@gmail.com>
2025-01-03 19:53:47 +1100

    re-created project to include sample page

--- HitomiScrollViewerWebApp/Program.cs
+++ HitomiScrollViewerWebApp/Program.cs
@@ -1,67 +1,49 @@
 using HitomiScrollViewerWebApp.Components;
-using System.Net.Sockets;
-using System.Net;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
-namespace HitomiScrollViewerWebApp {
-    public static class Program {
-        public static WebApplication CreateWebApplication() {
-            var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder();
 
-            builder.WebHost.UseUrls(["http://localhost:" + FreeTcpPort()]);
+builder.WebHost.UseUrls(["http://localhost:" + FreeTcpPort()]);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment()) {
-                app.UseExceptionHandler("/Error", createScopeForErrors: true);
-            }
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-
-            return app;
-        }
-
-        public static void Main(string[] args) {
-            var app = CreateWebApplication();
-            Task runTask = app.RunAsync();
-            OpenBrowser(app.Urls.First());
-            runTask.Wait();
-        }
-
-        public static int FreeTcpPort() {
-            TcpListener l = new(IPAddress.Loopback, 0);
-            l.Start();
-            int port = ((IPEndPoint)l.LocalEndpoint).Port;
-            l.Stop();
-            return port;
-        }
-
-        public static void OpenBrowser(string url) {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-                Process.Start("xdg-open", url);
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-                Process.Start("open", url);
-            }
-        }
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
-//string localPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-//Console.WriteLine(localPath);
-//Directory.CreateDirectory(Path.Combine(localPath, "Hitomi-server-naniiii"));
 
+app.UseAntiforgery();
 
-//app.Run();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+Task runTask = app.RunAsync();
+OpenBrowser(app.Urls.First());
+runTask.Wait();
+
+static int FreeTcpPort() {
+    TcpListener l = new(IPAddress.Loopback, 0);
+    l.Start();
+    int port = ((IPEndPoint)l.LocalEndpoint).Port;
+    l.Stop();
+    return port;
+}
+
+static void OpenBrowser(string url) {
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+    } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+        Process.Start("xdg-open", url);
+    } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+        Process.Start("open", url);
+    }
+}
