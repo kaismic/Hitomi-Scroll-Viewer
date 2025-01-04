0d1a97070be850039eb40d1f3218d0576f76364f
Isac Kim <isac08270@gmail.com>
2025-01-04 12:47:08 +1100

    removed auto-port assignment and added browse component

--- HitomiScrollViewerWebApp/Program.cs
+++ HitomiScrollViewerWebApp/Program.cs
@@ -1,12 +1,8 @@
 using HitomiScrollViewerWebApp.Components;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder();
 
-builder.WebHost.UseUrls(["http://localhost:" + FreeTcpPort()]);
+builder.WebHost.UseUrls();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -19,31 +15,10 @@ if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-Task runTask = app.RunAsync();
-OpenBrowser(app.Urls.First());
-runTask.Wait();
-
-static int FreeTcpPort() {
-    TcpListener l = new(IPAddress.Loopback, 0);
-    l.Start();
-    int port = ((IPEndPoint)l.LocalEndpoint).Port;
-    l.Stop();
-    return port;
-}
-
-static void OpenBrowser(string url) {
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-    } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-        Process.Start("xdg-open", url);
-    } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-        Process.Start("open", url);
-    }
-}
+app.Run();
